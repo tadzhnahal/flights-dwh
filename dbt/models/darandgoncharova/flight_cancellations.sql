256429bad360047352164d51ec766d2e85851751
goncharovad <goncharovad00@gmail.com>
2026-06-07 18:06:28 +0500

    Add DM flight_cancellations.sql
    sql скрипт витрины для анализа отмен вылетов самолетов

---
+++ dbt/models/darandgoncharova/flight_cancellations.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='table',
+    schema='team_vdga_dm'
+) }}
+
+select
+    sched_dttm_local::date as flight_date,
+    extract(hour from sched_dttm_local)::int as flight_hour,
+    extract(dow from sched_dttm_local)::int as flight_day_of_week,
+    carrier_code,
+    origin_airport_dk,
+    origin_airport_type,
+    count(*) as total_cancelled_flights,
+    now() as processed_dttm
+from {{ ref('flights_cancelled') }}
+group by 1, 2, 3, 4, 5, 6
